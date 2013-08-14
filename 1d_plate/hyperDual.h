f50c715ffbb879feba5ab08fcb6c1bee282b5b5e
sedire <kolovert@bk.ru>
2013-08-14 20:52:44 +0400

    added hyperduals

---
+++ 1d_plate/hyperDual.h
@@ -0,0 +1,134 @@
+#ifndef _PLATE_1D_HYPERDUAL_
+#define _PLATE_1D_HYPERDUAL_ 1
+
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::ostream;
+
+template<class D_PRES, int NN>
+class HPD;
+
+//--------------------------------------------------------------------
+// addition
+
+template<class D_PRES, int NN>
+HPD<D_PRES, NN> operator+( const HPD<D_PRES, NN>& lhs, const HPD<D_PRES, NN>& rhs )
+{
+	HPD<D_PRES, NN> ret;
+
+	for( int i = 0; i < NN + 1; ++i )
+	{
+		ret.elems[i] = lhs.elems[i] + rhs.elems[i];
+	}
+	return ret;
+}
+
+template<class D_PRES, int NN>
+HPD<D_PRES, NN> operator+( const D_PRES& lhs, const HPD<D_PRES, NN>& rhs )
+{
+	HPD<D_PRES, NN> ret;
+
+	for( int i = 0; i < NN + 1; ++i )
+	{
+		ret.elems[i] = rhs.elems[i];
+	}
+	ret.elems[0] += lhs;
+	return ret;
+}
+
+template<class D_PRES, int NN>
+HPD<D_PRES, NN> operator+( const HPD<D_PRES, NN>& lhs, const D_PRES& rhs )
+{
+	HPD<D_PRES, NN> ret;
+
+	for( int i = 0; i < NN + 1; ++i )
+	{
+		ret.elems[i] = lhs.elems[i];
+	}
+	ret.elems[0] += rhs;
+	return ret;
+}
+
+//----------------------------------------------------------------
+// subtraction
+
+template<class D_PRES, int NN>
+HPD<D_PRES, NN> operator-( const HPD<D_PRES, NN>& lhs, const HPD<D_PRES, NN>& rhs )
+{
+	HPD<D_PRES, NN> ret;
+
+	for( int i = 0; i < NN + 1; ++i )
+	{
+		ret.elems[i] = lhs.elems[i] - rhs.elems[i];
+	}
+	return ret;
+}
+
+template<class D_PRES, int NN>
+HPD<D_PRES, NN> operator-( const D_PRES& lhs, const HPD<D_PRES, NN>& rhs )
+{
+	HPD<D_PRES, NN> ret;
+
+	for( int i = 0; i < NN + 1; ++i )
+	{
+		ret.elems[i] = -rhs.elems[i];
+	}
+	ret.elems[0] += lhs;
+	return ret;
+}
+
+template<class D_PRES, int NN>
+HPD<D_PRES, NN> operator-( const HPD<D_PRES, NN>& lhs, const D_PRES& rhs )
+{
+	HPD<D_PRES, NN> ret;
+
+	for( int i = 0; i < NN + 1; ++i )
+	{
+		ret.elems[i] = lhs.elems[i];
+	}
+	ret.elems[0] -= rhs;
+	return ret;
+}
+
+//----------------------------------------------------------------
+// cout
+
+template<class D_PRES, int NN>
+ostream& operator<<( ostream& os, const HPD<D_PRES, NN>& item )
+{
+	for( int i = 0; i < NN; ++i )
+	{
+		os << item.elems[i] << " ";
+	}
+	os << item.elems[NN];
+	return os;
+}
+
+//----------------------------------------------------------------
+// class itself
+
+template<class D_PRES, int NN>
+class HPD		//HPD = hyper dual
+{
+public:
+	vector<D_PRES> elems;
+
+	HPD() {	elems.resize( NN + 1, 0.0 ); }
+	HPD( D_PRES a ) {	elems.resize( NN + 1, 0.0 ); elems[0] = a; }
+	D_PRES real() { return elems[0]; }
+
+	template<class D_PRES_, int NN_>
+	friend ostream& operator<<( ostream& os, const HPD& item );
+	
+	friend HPD operator+<>( const HPD& lhs, const HPD& rhs );
+	//friend HPD operator+<>( const D_PRES& lhs, const HPD& rhs );
+	//friend HPD operator+<>( const HPD& lhs, const D_PRES& rhs );
+
+	friend HPD operator-<>( const HPD& lhs, const HPD& rhs );
+	//friend HPD operator-<>( const D_PRES& lhs, const HPD& rhs );
+	//friend HPD operator-<>( const HPD& lhs, const D_PRES& rhs );
+};
+
+#endif
