5b28f36132c26d38746ef141f8ff832ae4ca4415
sedire <kolovert@bk.ru>
2013-09-27 19:55:13 -0500

    hyperd2 implemented

--- 1d_plate/main.cpp
+++ 1d_plate/main.cpp
@@ -4,6 +4,7 @@
 #include "Plate.h"
 #include "Optimizer.h"
 #include "hyperDual.h"
+#include "hyperDual2.h"
 #include <omp.h>
 
 using std::cout;
@@ -12,6 +13,32 @@ using std::endl;
 int main()
 {
 	cout << "hello\n";
+
+	HPD2<N_PRES, 4> xx1;
+	HPD2<N_PRES, 4> xx2;
+	HPD2<N_PRES, 4> xx3;
+	HPD2<N_PRES, 4> xx4;
+
+	HPD2<N_PRES, 4> yy;
+	
+	xx1.elems[0] = 5.01;
+	xx1.elems[1] = 1.0;
+	xx2.elems[0] = 23.22;
+	xx2.elems[2] = 1.0;
+	xx3.elems[0] = 4.36;
+	xx3.elems[3] = 1.0;
+	xx4.elems[0] = 1.6;
+	xx4.elems[4] = 1.0;
+
+	yy = xx1 * xx1 + xx2 * xx2 + xx3 * xx3 * xx3 + 2.0l * ( xx1 + xx2 ) + xx4 * ( xx3 - xx2 ) * ( xx3 - xx2 ) * ( xx3 - xx2 ) * ( xx2 - xx1 ) + 20.0l * xx4 * ( xx1 + xx3 ) * ( xx1 + xx3 ) + xx4 * xx4 * xx4 * xx4
+		+ 19.0l / xx3 / xx3 / xx3 / xx3 / xx3 - ( xx1 + xx2 + xx3 ) * ( xx2 - 1.0l ) / ( xx4 + xx2 ) / ( xx4 + xx2 )
+		+ xx1 * xx1 * ( xx2 - xx3 ) * sin( xx1 + xx2 * cos( exp( xx3 / ( xx4 + xx2 ) / sqrt( xx1 ) ) ) - sqrt( xx2 + xx3 ) );
+
+	cout << yy << endl;
+
+	std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
+	return 0;
+
 
 	omp_set_num_threads( 2 );
 
