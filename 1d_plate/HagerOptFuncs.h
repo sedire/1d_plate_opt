3d7f14ac88acdb94833037ac73100ceaf30f88ee
sedire <kolovert@bk.ru>
2013-10-12 14:25:28 -0500

    start removing plate new added

---
+++ 1d_plate/HagerOptFuncs.h
@@ -0,0 +1,18 @@
+#ifndef _PLATE_1D_HAGEROPTFUNCS_
+#define _PLATE_1D_HAGEROPTFUNCS_ 1
+
+#include "asa_user.h"
+
+double calc1stOrdOptInfoCG_DES( double* g, double* x, long n );
+
+//double calcValCG_DES( double* x, long n );
+
+//void calcGradCG_DES( double* g, double* x, long n );
+
+double calc1stOrdOptInfoASA( asa_objective* asa );
+
+double calcValASA( asa_objective* asa );
+
+void calcGradASA( asa_objective* asa );
+
+#endif
